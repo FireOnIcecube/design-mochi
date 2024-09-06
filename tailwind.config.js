4bd9a10e4f2a6b99ef612556751b803efc6c6f58
FireOnIcecube <z03888648@gmail.com>
2024-09-06 20:54:02 +0800

    tailwind 基本設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#3490dc', // 主色
+          dark: '#2779bd', // 深色版本
+          light: '#6cb2eb' // 淺色版本
+        },
+        secondary: {
+          DEFAULT: '#755540', // 主色
+          dark: '#2779bd', // 深色版本
+          light: '#6cb2eb' // 淺色版本
+        },
+        background: {
+          DEFAULT: '#FEF7EC',
+          light: '#FEF7EC', // 淺色背景
+          dark: '#1a202c' // 深色背景
+        }
+      }
+    }
   },
   variants: {
     extend: {}
