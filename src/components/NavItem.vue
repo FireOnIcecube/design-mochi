77f0d5f7a4d53b6bb42b10c5b0b618ca5a4ce5c2
FireOnIcecube <z03888648@gmail.com>
2024-09-21 12:44:08 +0800

    加入基本動態

---
+++ src/components/NavItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="tsx">
+import { Icon } from '@iconify/vue'
+import type { JsxElement } from 'typescript'
+
+const props = defineProps<{ title: string; primaryColor: string; icon: string }>()
+</script>
+
+<template>
+  <div
+    :class="props.primaryColor"
+    class="flex items-center justify-end gap-2 py-2.5 px-4 z-10 rounded-r-full cursor-pointer peer"
+  >
+    <div class="text-white font-semibold tracking-wide text-lg lg:text-xl">{{ props.title }}</div>
+
+    <Icon :icon="props.icon" class="text-white text-6xl lg:text-4xl" />
+  </div>
+</template>
+
+<style lang="css" scoped></style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,51 +1,79 @@
-<script setup lang="tsx"></script>
+<script setup lang="tsx">
+import NavItem from '@/components/NavItem.vue'
+import { Icon } from '@iconify/vue'
+
+const navItems: { title: string; primaryColor: string; icon: string }[] = [
+  {
+    title: '字體資源',
+    primaryColor: 'bg-sky-500',
+    icon: 'ic:round-font-download'
+  },
+  {
+    title: '字卡資源',
+    primaryColor: 'bg-red-500',
+    icon: 'ic:round-font-download'
+  },
+  {
+    title: '封面資源',
+    primaryColor: 'bg-yellow-500',
+    icon: 'ic:round-font-download'
+  }
+]
+</script>
 
 <template>
-  <div class="flex flex-col flex-1">
-    <header class="mt-4 mx-20 flex justify-end gap-6">
-      <router-link to="/dashboard"> <div class="font-bold">進入網站</div> </router-link>
+  <div class="flex flex-col flex-1 relative overflow-hidden">
+    <header class="mt-4 mx-10 md:mx-15 lg:mx-20 flex justify-end gap-6 z-10">
+      <router-link to="/fonts"> <div class="font-bold">進入網站</div> </router-link>
       <div class="font-bold">關於我們</div>
     </header>
 
     <div class="flex flex-row flex-1">
-      <div class="flex flex-col justify-center gap-4">
-        <div
-          class="flex items-center justify-end bg-[#755540] w-16 h-16 rounded-r-full cursor-pointer"
+      <div class="flex flex-col justify-center gap-8">
+        <!-- <div
+          class="flex items-center justify-end gap-2 py-2.5 px-4 z-10 bg-sky-500 rounded-r-full cursor-pointer peer"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="2.5rem"
-            height="2.5rem"
-            viewBox="0 0 24 24"
-            class="mr-4"
-          >
-            <path
-              fill="white"
-              d="M9.93 13.5h4.14L12 7.98zM20 2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2m-4.29 15.88l-.9-2.38H9.17l-.89 2.37a.968.968 0 1 1-1.81-.69l4.25-10.81c.22-.53.72-.87 1.28-.87s1.06.34 1.27.87l4.25 10.81a.968.968 0 0 1-.9 1.32c-.4 0-.76-.25-.91-.62"
-            />
-          </svg>
-        </div>
-        <div
+          <div class="text-white font-semibold tracking-wide text-lg lg:text-xl">字體資源</div>
+
+          <Icon icon="ic:round-font-download" class="text-white text-6xl lg:text-4xl" />
+        </div> -->
+
+        <NavItem
+          v-for="(item, index) in navItems"
+          :key="index"
+          :title="item.title"
+          :primaryColor="item.primaryColor"
+          :icon="item.icon"
+        />
+
+        <span
+          class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-20 w-20 rounded-full bg-transparent transition-all duration-500 peer-hover:bg-red-500 peer-hover:opacity-50 peer-hover:scale-[30]"
+        ></span>
+
+        <!-- <div
+         peer-hover:bg-[#b3d3e7]
           class="flex items-center justify-end bg-[#F8C1E1] w-16 h-16 rounded-r-full cursor-pointer"
         ></div>
 
         <div
           class="flex items-center justify-end bg-[#F0E4CB] w-16 h-16 rounded-r-full cursor-pointer"
         ></div>
-
         <div
           class="flex items-center justify-end bg-[#ED1C24] w-16 h-16 rounded-r-full cursor-pointer"
         ></div>
         <div
           class="flex items-center justify-end bg-[#82B8D9] w-16 h-16 rounded-r-full cursor-pointer"
-        ></div>
+        ></div> -->
       </div>
 
-      <div class="container mx-auto flex items-center justify-center">
-        <div v-motion-fade class="">
-          <h1 class="text-9xl font-[900] md:text-[12rem]">你好世界</h1>
-        </div>
+      <div class="container flex items-center justify-center">
+        <span v-motion-fade>
+          <h1 class="text-9xl font-[900] md:text-[12rem] inline-block text-white">你好</h1>
+          <h1 class="text-9xl font-[900] md:text-[12rem] inline-block">世界</h1>
+        </span>
       </div>
     </div>
   </div>
 </template>
+
+<style lang="css" scoped></style>
