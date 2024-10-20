d684aa364f85249b7dc754c8a610abfcb5f4659b
FireOnIcecube <z03888648@gmail.com>
2024-10-20 22:47:09 +0800

    重構 layout

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -4,14 +4,14 @@ import Header from '@/components/Header.vue'
 </script>
 
 <template>
-  <div class="flex h-screen bg-gray-200 font-roboto">
+  <div class="flex h-screen bg-[#FEF7EC]">
     <Sidebar />
 
-    <div class="flex-1 flex flex-col overflow-hidden">
+    <div class="flex-grow flex flex-col">
       <Header />
 
-      <main class="flex-1 overflow-x-hidden overflow-y-auto bg-[#FEF7EC] pl-4 md:pl-8 lg:pl-12">
-        <div class="container mx-auto px-6 py-8">
+      <main class="overflow-x-hidden overflow-y-auto px-6 py-8">
+        <div class="container mx-auto">
           <slot />
         </div>
       </main>
