15c8d3eac855f9a4cf4d8f47ac0f21e6f83f2fc3
FireOnIcecube <z03888648@gmail.com>
2025-05-01 18:37:41 +0800

    更新 HomePage.vue 中的佈局，將 justify-items-center 更改為 place-content-center 以改善內容對齊

--- src/views/frontend/HomePage.vue
+++ src/views/frontend/HomePage.vue
@@ -4,7 +4,7 @@ import ThumbnailCard from '@/components/ThumbnailCard.vue'
 
 <template>
   <div
-    class="grid grid-cols-1 justify-items-center gap-x-8 gap-y-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+    class="grid grid-cols-1 place-content-center gap-x-8 gap-y-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
   >
     <ThumbnailCard />
     <ThumbnailCard />
