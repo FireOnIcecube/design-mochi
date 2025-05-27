e76fad717d40be54ae1c103c8b2d4385a93625ed
FireOnIcecube <z03888648@gmail.com>
2025-05-27 15:41:45 +0800

    feat(SearchDialog): 添加搜索对话框和输入组件
    - 重命名切换函数为 toggleDialog
    - 添加搜索输入框和按钮
    - 更新对话框标题样式
    - 优化 DialogTrigger 组件的使用

--- apps/client/src/components/SearchDialog.vue
+++ apps/client/src/components/SearchDialog.vue
@@ -11,9 +11,11 @@ import {
   DialogTrigger
 } from '@client/components/ui/dialog'
 import { Button } from '@client/components/ui/button'
+import { Search } from 'lucide-vue-next'
+import { Input } from '@client/components/ui/input'
 
 const isOpen = ref(false)
-const toggleDrawer = () => {
+const toggleDialog = () => {
   isOpen.value = !isOpen.value
 }
 </script>
@@ -21,7 +23,7 @@ const toggleDrawer = () => {
 <template>
   <Dialog>
     <DialogTrigger as-child>
-      <div @click="toggleDrawer" class="p-4">
+      <div @click="toggleDialog" class="p-4">
         <Icon
           icon="ion:search"
           width="1.8rem"
@@ -34,13 +36,26 @@ const toggleDrawer = () => {
       class="md:max-w-1/2 min-h-1/2 max-h-[90dvh] grid-rows-[auto_minmax(0,1fr)_auto] p-0 sm:max-w-[425px]"
     >
       <DialogHeader class="p-6 pb-0">
-        <DialogTitle class="text-2xl">尋找封面</DialogTitle>
+        <DialogTitle class="text-3xl">尋找封面</DialogTitle>
         <!-- <DialogDescription>
           Make changes to your profile here. Click save when you're done.
         </DialogDescription> -->
       </DialogHeader>
       <div class="grid h-max gap-4 overflow-y-auto px-6 py-4">
         <div class="flex flex-col justify-between">
+          <!-- test -->
+
+          <div class="flex w-full max-w-sm items-center justify-center gap-1.5">
+            <div class="relative w-full max-w-sm items-center">
+              <Input id="search" type="text" placeholder="Search..." class="pl-10" />
+              <span class="absolute inset-y-0 start-0 flex items-center justify-center px-2">
+                <Search class="text-muted-foreground size-6" />
+              </span>
+            </div>
+
+            <Button type="submit"> Subscribe </Button>
+          </div>
+
           <p>
             This is some placeholder content to show the scrolling behavior for modals. We use
             repeated line breaks to demonstrate how content can exceed minimum inner height, thereby
@@ -49,9 +64,16 @@ const toggleDrawer = () => {
           </p>
 
           <p>This content should appear at the bottom after you scroll.</p>
+
+          <div class="p-4 font-bold">isOpen: {{ isOpen }}</div>
         </div>
       </div>
-      <DialogFooter class="p-6 pt-0"> <Button type="submit"> Save changes </Button> </DialogFooter>
+
+      <DialogFooter class="p-6 pt-0">
+        <DialogTrigger asChild>
+          <Button type="submit" @click="toggleDialog"> Save changes </Button></DialogTrigger
+        >
+      </DialogFooter>
     </DialogContent>
   </Dialog>
 </template>
