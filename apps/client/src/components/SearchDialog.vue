58fa2e68d876218a5d70a833970ab8597e49b982
FireOnIcecube <z03888648@gmail.com>
2025-05-31 17:27:34 +0800

    feat(SearchDialog): 调整复选框样式以改善可读性和用户体验

--- apps/client/src/components/SearchDialog.vue
+++ apps/client/src/components/SearchDialog.vue
@@ -137,13 +137,13 @@ const toggleDialog = () => {
 
           <div class="border-outline dark:border-outline-dark w-full grow overflow-y-auto border-2">
             <TabsContent value="類別一" class="h-full">
-              <div class="mx-auto h-full overflow-y-auto px-8 py-12 xl:w-3/4">
-                <div class="grid grid-cols-2 gap-y-8 md:grid-cols-3 lg:grid-cols-4">
+              <div class="mx-auto h-full overflow-y-auto px-6 py-12 xl:w-3/4">
+                <div class="grid grid-cols-2 gap-x-2 gap-y-8 md:grid-cols-3 lg:grid-cols-4">
                   <div class="col-span-full flex items-center space-x-2">
                     <Checkbox id="terms" />
                     <label
                       for="terms"
-                      class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      class="lg:text-md font-notosans text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 xl:text-lg"
                     >
                       全選
                     </label>
@@ -152,7 +152,7 @@ const toggleDialog = () => {
                     <Checkbox id="A" />
                     <label
                       for="A"
-                      class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      class="lg:text-md font-notosans text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 xl:text-lg"
                     >
                       測試項目 A
                     </label>
@@ -161,7 +161,7 @@ const toggleDialog = () => {
                     <Checkbox id="B" />
                     <label
                       for="B"
-                      class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      class="lg:text-md font-notosans text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 xl:text-lg"
                     >
                       測試項目 B
                     </label>
