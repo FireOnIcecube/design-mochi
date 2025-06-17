64ba7469953f1edda7cd5206b41c7172d3acc0c0
FireOnIcecube <z03888648@gmail.com>
2025-06-17 09:27:48 +0800

    feat(TestPage): 添加图片上传功能和预览

--- apps/client/src/views/TestPage.vue
+++ apps/client/src/views/TestPage.vue
@@ -1,11 +1,17 @@
-<template>
-  <SearchForm :filterConfig="filterConfig" :modelValue="filterModel" @submit="handleSubmit" />
-</template>
-
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import SearchForm from '@client/components/SearchForm.vue'
+
+import { storage } from '../firebase'
+import {
+  getStorage,
+  ref as storageRef,
+  uploadBytesResumable,
+  getDownloadURL,
+  uploadBytes
+} from 'firebase/storage'
+import imageCompression from 'browser-image-compression'
 
 // ✅ 從資料庫取得的篩選欄位與對應選項
 const filterConfig = [
@@ -57,4 +63,53 @@ const handleSubmit = (model) => {
   }
   router.push({ query: newQuery })
 }
+
+// 上傳圖片
+const selectedFile = ref<File | null>(null)
+const imgUrl = ref<String>('')
+const previewUrl = ref<string>('')
+
+function handleChange(e: Event) {
+  const file = (e.target as HTMLInputElement).files?.[0]
+  if (file) {
+    URL.revokeObjectURL(previewUrl.value)
+
+    selectedFile.value = file
+    previewUrl.value = URL.createObjectURL(selectedFile.value)
+  }
+}
+
+async function uploadImage() {
+  if (!selectedFile.value) return
+
+  try {
+    const filename = selectedFile.value.name
+    const fileRef = storageRef(storage, `uploads/${filename}`)
+    const snapshot = await uploadBytes(fileRef, selectedFile.value)
+
+    console.log('Uploaded!', snapshot.ref)
+    imgUrl.value = await getDownloadURL(snapshot.ref)
+  } catch (err) {
+    console.error('上傳圖片失敗: ', err)
+  }
+}
 </script>
+
+<template>
+  <SearchForm :filterConfig="filterConfig" :modelValue="filterModel" @submit="handleSubmit" />
+  <hr />
+
+  <div class="mt-20 space-y-4 p-4">
+    <h1 class="text-xl font-bold">圖片上傳 MVP</h1>
+    <input accept="image/*" type="file" @change="handleChange" />
+    <button
+      :disabled="!selectedFile"
+      class="rounded bg-green-500 px-4 py-2 text-white"
+      @click="uploadImage"
+    >
+      上傳
+    </button>
+
+    <img :src="previewUrl" />
+  </div>
+</template>
