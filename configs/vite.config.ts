4cd18f52a6dd51e81d7c3ad3930811a08531ffb8
FireOnIcecube <z03888648@gmail.com>
2025-06-19 13:28:26 +0800

    feat(firebase): 添加对新包路径的支持

--- configs/vite.config.ts
+++ configs/vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('../', import.meta.url)),
       '@client': fileURLToPath(new URL('../apps/client/src', import.meta.url)),
       '@admin': fileURLToPath(new URL('../apps/admin/src', import.meta.url)),
-      '@configs': fileURLToPath(new URL('../configs', import.meta.url))
+      '@configs': fileURLToPath(new URL('../configs', import.meta.url)),
+      '@pkg/firebase': fileURLToPath(new URL('../../packages/firebase', import.meta.url))
     }
   }
 })
