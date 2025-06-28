966f2f9bb8533cadee5920ebbbd9b37f13c1dbcd
FireOnIcecube <z03888648@gmail.com>
2025-06-28 19:54:39 +0800

    feat(dark-mode): 添加主题切换功能和相关依赖
    - 新增 daisyui 依赖以支持主题样式
    - 创建 useDarkMode 组合式 API 以管理暗黑模式
    - 更新 NavHeader 组件以实现主题切换功能

---
+++ apps/admin/src/components/NavHeader.vue
@@ -0,0 +1,43 @@
+<script setup lang="tsx">
+import { ref, watch } from 'vue'
+import { Icon } from '@iconify/vue'
+import { useDarkMode } from '@client/composables/useDarkMode'
+
+const { isDarkMode, toggleDarkMode } = useDarkMode()
+</script>
+
+<template>
+  <div
+    class="bg-surface dark:bg-surface-dark transition-theme flex items-center justify-between md:mx-4"
+  >
+    <router-link
+      to="/"
+      class="font-mantou text-content dark:text-content-dark text-2xl hover:opacity-80 lg:text-4xl"
+    >
+      設計麻糬
+    </router-link>
+
+    <div>
+      <nav class="flex gap-2 lg:mr-4">
+        <!-- <router-link
+            to="/contact"
+            class="text-content hover:text-gray-500 dark:text-content-dark dark:hover:text-gray-400"
+          >
+            我的收藏
+          </router-link> -->
+
+        <label class="swap swap-rotate text-content dark:text-content-dark">
+          <input type="checkbox" class="theme-controller hidden" @change="() => toggleDarkMode()" />
+
+          <Icon icon="uil:sun" class="swap-off h-6 w-6 fill-current lg:h-8 lg:w-8" />
+
+          <Icon icon="uil:moon" class="swap-on h-6 w-6 fill-current lg:h-8 lg:w-8" />
+        </label>
+
+        <!-- <BurgerMenu /> -->
+      </nav>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped></style>
