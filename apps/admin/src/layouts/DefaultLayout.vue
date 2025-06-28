e8ef595e8f0045b56c14e940b47fa9eb43056649
FireOnIcecube <z03888648@gmail.com>
2025-06-28 19:54:29 +0800

    feat(package): 添加 @iconify/vue 依赖并更新相关锁定文件
    feat(layout): 更新 DefaultLayout 以引入 Firebase 身份验证

--- apps/admin/src/layouts/DefaultLayout.vue
+++ apps/admin/src/layouts/DefaultLayout.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getAuth, signOut } from 'firebase/auth'
+import NavHeader from '@client/components/NavHeader.vue'
+
+const auth = getAuth()
+</script>
 
 <template>
   <div class="flex min-h-screen flex-col">
+    <!-- <header>
+      <div>{{ auth.currentUser?.displayName }}</div>
+      <div>{{ auth.currentUser?.email }}</div>
+    </header> -->
+
+    <header class="border-b px-4 lg:p-4 dark:border-gray-700">
+      <NavHeader />
+    </header>
+
     <header>共用導航</header>
 
     <router-view />
