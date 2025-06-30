f914fd2ac27e11d2ef861d112df5055ac41dc3b0
FireOnIcecube <z03888648@gmail.com>
2025-06-30 16:04:00 +0800

    feat(Sidebar): 更新侧边栏布局样式以改善用户体验

--- apps/admin/src/components/Sidebar.vue
+++ apps/admin/src/components/Sidebar.vue
@@ -8,14 +8,21 @@ const links = [
 </script>
 
 <template>
-  <ul class="h-full w-64 bg-gray-100 p-4 dark:bg-gray-800">
-    <li v-for="link in links" :key="link.path" class="mb-2">
-      <router-link
-        :to="{ path: link.path }"
-        class="block rounded px-4 py-2 text-gray-700 transition-colors hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-gray-700"
-      >
-        {{ link.name }}
-      </router-link>
-    </li>
-  </ul>
+  <div class="flex h-full w-64 flex-col bg-gray-100 p-4 dark:bg-gray-800">
+    <ul class="flex-1">
+      <li v-for="link in links" :key="link.path" class="mb-2">
+        <router-link
+          :to="{ path: link.path }"
+          class="block cursor-pointer rounded px-4 py-2 text-gray-700 transition-colors select-none hover:bg-gray-200 active:scale-95 dark:text-gray-300 dark:hover:bg-gray-700"
+        >
+          <span class="text-lg font-black"> {{ link.name }} </span>
+        </router-link>
+      </li>
+    </ul>
+    <button
+      class="bg-surface-dark dark:bg-surface cursor-pointer rounded-lg py-2 shadow-md transition-colors hover:bg-gray-700 active:scale-95 active:shadow-inner dark:hover:bg-gray-200"
+    >
+      <span class="text-content-dark dark:text-content font-notosans text-lg">登出</span>
+    </button>
+  </div>
 </template>
