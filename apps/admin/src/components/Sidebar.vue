0ce6620c79d787f0da9c54ac7101dfbc385df2fe
FireOnIcecube <z03888648@gmail.com>
2025-07-02 18:56:14 +0800

    feat(Sidebar): 更新侧边栏菜单项以包含管理封面和管理类别链接

--- apps/admin/src/components/Sidebar.vue
+++ apps/admin/src/components/Sidebar.vue
@@ -27,31 +27,52 @@ const links = [
   { label: '管理類別', path: '/categories' },
 ]
 
-// const menuItems = [
-//   {
-//     title: '管理封面',
-//     children: [
-//       { label: '子頁面 A1', path: '/a/a1' },
-//       { label: '子頁面 A2', path: '/a/a2' },
-//     ],
-//   },
-//   {
-//     title: '管理類別',
-//     children: [
-//       { label: '子頁面 B1', path: '/b/b1' },
-//       { label: '子頁面 B2', path: '/b/b2' },
-//     ],
-//   },
-// ]
+const menuItems = [
+  {
+    title: '管理封面',
+    children: [
+      { label: '封面列表', path: '/thumbnails' },
+      { label: '增加封面', path: '/thumbnails/create' },
+    ],
+  },
+  {
+    title: '管理類別',
+    children: [
+      { label: '管理類別', path: '/categories' },
+      { label: '增加類別', path: '/categories/create' },
+    ],
+  },
+]
 </script>
 
 <template>
   <div class="relative flex h-full">
     <div
-      class="fixed flex flex-1 flex-col overflow-hidden bg-gray-100 transition-all duration-300 ease-in-out md:static dark:bg-gray-800"
+      class="fixed overflow-hidden bg-gray-100 transition-all duration-300 ease-in-out md:static dark:bg-gray-800"
       :class="isOpen ? ['w-64', 'p-4'] : 'w-0'"
     >
-      <ul class="mt-10 flex-1 md:mt-0" :class="{ hidden: !isOpen }">
+      <div class="flex h-full w-full flex-col" :class="{ hidden: !isOpen }">
+        <ul class="mt-10 flex-1 md:mt-0">
+          <li v-for="link in links" :key="link.path" class="mb-2">
+            <router-link
+              :to="{ path: link.path }"
+              class="block cursor-pointer rounded px-4 py-2 text-gray-700 transition-colors select-none hover:bg-gray-200 active:scale-95 dark:text-gray-300 dark:hover:bg-gray-700"
+              :class="{ 'bg-gray-200 dark:bg-gray-700': isActive(link.path) }"
+            >
+              <span class="text-lg font-black"> {{ link.label }} </span>
+            </router-link>
+          </li>
+        </ul>
+
+        <button
+          class="bg-surface-dark dark:bg-surface cursor-pointer rounded-lg py-2 shadow-md transition-colors hover:bg-gray-700 active:scale-95 active:shadow-inner dark:hover:bg-gray-200"
+          @click="logout"
+        >
+          <span class="text-content-dark dark:text-content font-notosans text-lg">登出</span>
+        </button>
+      </div>
+
+      <!-- <ul class="mt-10 flex-1 md:mt-0" :class="{ hidden: !isOpen }">
         <li v-for="link in links" :key="link.path" class="mb-2">
           <router-link
             :to="{ path: link.path }"
@@ -68,7 +89,7 @@ const links = [
         @click="logout"
       >
         <span class="text-content-dark dark:text-content font-notosans text-lg">登出</span>
-      </button>
+      </button> -->
     </div>
 
     <div
