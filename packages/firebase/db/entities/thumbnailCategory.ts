1c86cf4eb1c1534bea5ef0f37da8076d923c7594
FireOnIcecube <z03888648@gmail.com>
2025-07-09 23:25:25 +0800

    feat(api) 新增 thumbnailCategory CRUD api

--- packages/firebase/db/entities/thumbnailCategory.ts
+++ packages/firebase/db/entities/thumbnailCategory.ts
@@ -1,9 +1,27 @@
-import { type FirestoreDataConverter } from 'firebase/firestore'
-import { ThumbnailCategory } from '@pkg/types'
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  QueryConstraint,
+  serverTimestamp,
+  setDoc,
+  type FirestoreDataConverter
+} from 'firebase/firestore'
+import {
+  ThumbnailCategory,
+  ThumbnailCategoryBase,
+  ThumbnailCategoryCreateData,
+  ThumbnailCategoryEditData
+} from '@pkg/types'
+import { db } from '@pkg/firebase/index'
 
 // firestore 轉換器
 export const thumbnailConverter: FirestoreDataConverter<ThumbnailCategory> = {
-  toFirestore(thumbnailCategory: ThumbnailCategory) {
+  toFirestore(thumbnailCategory) {
     const { id, ...data } = thumbnailCategory
     return data
   },
@@ -11,5 +29,85 @@ export const thumbnailConverter: FirestoreDataConverter<ThumbnailCategory> = {
   fromFirestore(snapshot, options) {
     const data = snapshot.data(options)
     return { ...data, id: snapshot.id } as ThumbnailCategory
+  }
+}
+
+const collectionRef = collection(db, 'thumbnail_categories').withConverter(thumbnailConverter)
+
+export async function fetchThumbnailCategory(...constraints: QueryConstraint[]) {
+  try {
+    const q = constraints.length > 0 ? query(collectionRef, ...constraints) : query(collectionRef)
+
+    const snapshot = await getDocs(q)
+
+    return snapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id
+    })) as ThumbnailCategory[]
+  } catch (e) {
+    throw e
+  }
+}
+
+export async function getThumbnailCategory(id: string) {
+  if (!id) throw new Error('無法找到封面類別，請稍後再試。')
+  const docRef = doc(collectionRef, id)
+
+  try {
+    const docSnap = await getDoc(docRef)
+    if (!docSnap.exists()) throw new Error('搜尋封面類別不存在')
+    return {
+      ...docSnap.data(),
+      id: docSnap.id
+    } as ThumbnailCategory
+  } catch (e) {
+    throw e
+  }
+}
+
+export async function createThumbnailCategory(value: ThumbnailCategoryCreateData) {
+  try {
+    await addDoc(collectionRef, {
+      ...value,
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp()
+    } as ThumbnailCategory)
+  } catch (e) {
+    throw e
+  }
+}
+
+export async function editThumbnailCategory(id: string, value: ThumbnailCategoryEditData) {
+  if (!id) throw new Error('無法編輯封面類別，請稍後再試。')
+
+  const editedDocRef = doc(collectionRef, id)
+  try {
+    const editedDoc = await getDoc(editedDocRef)
+    if (!editedDoc.exists()) throw new Error('編輯封面類別不存在')
+
+    await setDoc(editedDocRef, {
+      ...value,
+      updatedAt: serverTimestamp()
+    } as ThumbnailCategory)
+  } catch (e) {
+    throw e
+  }
+}
+
+export async function deleteCategory(id: string) {
+  if (!id) throw new Error('無法刪除封面類別，請稍後再試。')
+
+  const deletedDocRef = doc(collectionRef, id)
+
+  if (confirm('確定要刪除這個分類嗎？')) {
+    try {
+      const deletedDoc = await getDoc(deletedDocRef)
+      if (!deletedDoc.exists()) throw new Error('刪除封面類別不存在')
+
+      await deleteDoc(deletedDocRef)
+      console.log(`刪除成功，文件: ${id}`)
+    } catch (e) {
+      throw e
+    }
   }
 }
