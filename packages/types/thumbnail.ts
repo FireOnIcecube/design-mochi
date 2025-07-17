88ecde94fbc7cbc15a4854a5ed026ce9516b4040
FireOnIcecube <z03888648@gmail.com>
2025-07-17 14:52:10 +0800

    修改(types/thumbnail): 將categories 設為必填

--- packages/types/thumbnail.ts
+++ packages/types/thumbnail.ts
@@ -3,7 +3,7 @@ import type { NonEmptyArray, WithId, WithTimestamps } from './common'
 export interface ThumbnailBase {
   name: string
   imageUrl: string
-  categories?: NonEmptyArray<{ category: string; tags: NonEmptyArray<string> }>
+  categories: NonEmptyArray<{ category: string; tags: NonEmptyArray<string> }>
 }
 
 export type Thumbnail = WithId<WithTimestamps<ThumbnailBase>>
