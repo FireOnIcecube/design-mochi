c5fa0e3bacb39254004fbd5b4c708f7972249b15
FireOnIcecube <z03888648@gmail.com>
2025-07-19 17:22:55 +0800

    修改(thumbnail): 整理導入語句並重構 createThumbnail 函數

--- packages/firebase/db/entities/thumbnail.ts
+++ packages/firebase/db/entities/thumbnail.ts
@@ -1,5 +1,12 @@
-import { type FirestoreDataConverter } from 'firebase/firestore'
-import { Thumbnail } from '../types'
+import {
+  collection,
+  doc,
+  serverTimestamp,
+  setDoc,
+  type FirestoreDataConverter
+} from 'firebase/firestore'
+import { db } from '@pkg/firebase'
+import { Thumbnail, ThumbnailBase } from '@pkg/types'
 
 // firestore 轉換器
 export const thumbnailConverter: FirestoreDataConverter<Thumbnail> = {
@@ -9,5 +16,20 @@ export const thumbnailConverter: FirestoreDataConverter<Thumbnail> = {
   fromFirestore(snapshot, options) {
     const data = snapshot.data(options)
     return { ...data } as Thumbnail
+  }
+}
+
+const collectionRef = collection(db, 'thumbnails').withConverter(thumbnailConverter)
+
+export async function createThumbnail(value: ThumbnailBase) {
+  const docRef = doc(collectionRef)
+  try {
+    await setDoc(docRef, {
+      ...value,
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp()
+    } as Thumbnail)
+  } catch (e) {
+    throw e
   }
 }
