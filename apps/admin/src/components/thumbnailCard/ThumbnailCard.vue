c4ff1e6408b2f0434533e1c57f5c63b955c11e2f
FireOnIcecube <z03888648@gmail.com>
2025-07-28 12:54:35 +0800

    feat(thumbnailCard): enhance thumbnail display with hover effects and improved layout

--- apps/admin/src/components/thumbnailCard/ThumbnailCard.vue
+++ apps/admin/src/components/thumbnailCard/ThumbnailCard.vue
@@ -7,22 +7,37 @@ const props = defineProps<{
 </script>
 
 <template>
-  <router-link
-    :to="{ name: 'ThumbnailDetail', params: { id: props.thumbnail.videoId } }"
-    class="group block"
-  >
-    <div class="aspect-video w-full">
-      <img
-        :src="props.thumbnail.imageUrl"
-        class="h-full w-full rounded object-cover"
-        alt="Thumbnail"
-      />
-    </div>
-
-    <p
-      class="font-notosans dark:text-content-dark group-hover:text-content-link dark:group-hover:text-content-link-dark mt-2 line-clamp-2 text-xs lg:text-lg"
+  <div class="group relative">
+    <!--  -->
+    <div
+      class="curosr-default absolute top-0 right-0 left-0 z-10 flex -translate-y-2 bg-black/40 opacity-0 transition-all duration-300 ease-in-out group-hover:translate-y-0 group-hover:opacity-100"
     >
       {{ props.thumbnail.name }}
-    </p>
-  </router-link>
+    </div>
+    <router-link :to="{ name: 'ThumbnailDetail', params: { id: props.thumbnail.videoId } }">
+      <div class="relative aspect-video w-full">
+        <div
+          class="absolute inset-0 transition duration-300 ease-in-out group-hover:bg-white/40"
+        ></div>
+        <img
+          :src="props.thumbnail.imageUrl"
+          class="h-full w-full rounded object-cover"
+          alt="Thumbnail"
+        />
+      </div>
+
+      <p
+        class="font-notosans dark:text-content-dark group-hover:text-content-link dark:group-hover:text-content-link-dark mt-2 line-clamp-2 text-xs lg:text-lg"
+      >
+        {{ props.thumbnail.name }}
+      </p>
+
+      <!-- <button
+      class="absolute top-2 right-2 hidden rounded-full bg-white p-1 text-red-500 shadow transition group-hover:block hover:bg-red-100"
+      title="刪除"
+    >
+      🗑️
+    </button> -->
+    </router-link>
+  </div>
 </template>
