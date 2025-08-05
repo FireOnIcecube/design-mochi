60ecda54dc6ad7d9d0a1f2979605c8233c0b31fe
FireOnIcecube <z03888648@gmail.com>
2025-08-05 22:15:10 +0800

    feat(thumbnailCategory): 新增 thumbnail_categories 文档更新功能

---
+++ packages/functions/src/firestore/thumbnailCategory/onThumnbailCategoryUpdate.ts
@@ -0,0 +1,18 @@
+// import { get } from 'axios'
+// import { getFirestore } from 'firebase-admin/firestore'
+// import { onDocumentDeleted, onDocumentUpdated } from 'firebase-functions/firestore'
+
+// export const onThumbnailUpdate = onDocumentUpdated(
+//   {
+//     document: 'thumbnail_categories/{id}',
+//     region: 'asia-east1'
+//   },
+//   async (event) => {
+//     const db = getFirestore()
+//     const snapshot = event.data
+//   }
+// )
+
+// async function name(params:type) {
+
+// }
