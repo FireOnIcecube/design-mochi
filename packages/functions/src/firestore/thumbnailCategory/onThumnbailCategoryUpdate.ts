82af559c426beb1410eac7fa59cf4f4d168251f7
FireOnIcecube <z03888648@gmail.com>
2025-08-06 14:51:15 +0800

    feat(onThumbnailCategoryUpdate):@pkg/functions 的 tsconfig 應用 根目錄的 tsconfig

--- packages/functions/src/firestore/thumbnailCategory/onThumnbailCategoryUpdate.ts
+++ packages/functions/src/firestore/thumbnailCategory/onThumnbailCategoryUpdate.ts
@@ -1,18 +1,19 @@
-// import { get } from 'axios'
-// import { getFirestore } from 'firebase-admin/firestore'
-// import { onDocumentDeleted, onDocumentUpdated } from 'firebase-functions/firestore'
+import { get } from 'axios'
+import { getFirestore } from 'firebase-admin/firestore'
+import { onDocumentDeleted, onDocumentUpdated } from 'firebase-functions/firestore'
+import type { ThumbnailTag } from '@pkg/types'
 
-// export const onThumbnailUpdate = onDocumentUpdated(
-//   {
-//     document: 'thumbnail_categories/{id}',
-//     region: 'asia-east1'
-//   },
-//   async (event) => {
-//     const db = getFirestore()
-//     const snapshot = event.data
-//   }
-// )
+export const onThumbnailUpdate = onDocumentUpdated(
+  {
+    document: 'thumbnail_categories/{id}',
+    region: 'asia-east1'
+  },
+  async (event) => {
+    const db = getFirestore()
+    const newValue = event.data?.after.data()
 
-// async function name(params:type) {
+    const newTags = newValue?.tags as ThumbnailTag | undefined
+  }
+)
 
-// }
+// async function replaceTagsFromThumbnail(slug)
