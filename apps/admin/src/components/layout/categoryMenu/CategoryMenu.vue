7cf96fb3fd3b304877c09fee70864f39ec09a23b
FireOnIcecube <z03888648@gmail.com>
2025-08-12 23:41:48 +0800

    feat(ui): CategoryMenu 的樣式設計

--- apps/admin/src/components/layout/categoryMenu/CategoryMenu.vue
+++ apps/admin/src/components/layout/categoryMenu/CategoryMenu.vue
@@ -7,8 +7,14 @@ const props = defineProps<{
 </script>
 <template>
   <div class="flex h-full w-full items-center justify-center p-4">
-    <div class="text-content dark:text-content-dark flex items-center justify-center gap-x-8 px-8">
-      <div v-for="cat in props.thumbnailCategories" :key="cat.id">
+    <div
+      class="text-content font-notosans dark:text-content-dark flex gap-x-8 overflow-x-auto bg-amber-400 bg-red-300 px-8 text-lg"
+    >
+      <div
+        v-for="cat in props.thumbnailCategories"
+        :key="cat.id"
+        class="min-w-[120px] flex-shrink-0"
+      >
         {{ cat.name }}
       </div>
     </div>
